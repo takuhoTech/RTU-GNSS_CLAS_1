2defdd32c6652163aa0b107178843b4949771188
takuhoTech <109070168+takuhoTech@users.noreply.github.com>
2023-11-12 18:09:07 +0900

    発注完了

--- Logo/Logo_CLAS_Receiver.py
+++ Logo/Logo_CLAS_Receiver.py
@@ -37,6 +37,8 @@ img_main = ImageOps.invert(img_main)
 img_main = img_main.point(lambda x: 0 if x < 150 else x)
 img_main, _ = scale_to_height(img_main, 150)
 
+img_main.save(dir + "Logo_CLAS.jpg")
+
 # kicad
 kicad = Image.open(dir + "kicad.png")
 
@@ -50,15 +52,17 @@ rmf = Image.open(dir + "rmf-nhk.png")
 # ublox
 ublox = Image.open(dir + "ublox.png")
 
-img = Image.new("1", (570, 160))
-img.paste(img_main, (0, 0))
-paste(img, 370, 68, 90, [ublox])
+offset = 150
+img = Image.new("1", (570-offset, 160))
+
+#img.paste(img_main, (0, 0))
+paste(img, 370-offset, 65, 80, [ublox])
 draw = ImageDraw.Draw(img)
 font1 = ImageFont.truetype(dir + "impact.ttf",  72)#75
 font2 = ImageFont.truetype(dir + "impact.ttf",  40)
-draw.text((152, -5), name, fill="white", font=font1)
-draw.text((156, 69), detail, fill="white", font=font2)
-draw.text((166, 106), detail2, fill="white", font=font2)
+draw.text((152-offset, -5), name, fill="white", font=font1)
+draw.text((156-offset, 69), detail, fill="white", font=font2)
+draw.text((166-offset, 106), detail2, fill="white", font=font2)
 
 #img.show()
 img.save(dir + "Logo_CLAS_Receiver.jpg")
